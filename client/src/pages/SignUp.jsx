78dbc0163f3ef7cdabe5bbd85df05ac739332e95
Ray Devera <rayd808@yahoo.com>
2024-07-09 16:18:16 -0700

    complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+// keeps track of input values in the input field
 export default function SignUp() {
   const [formData, setFormData] = useState({})
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData(
       {
@@ -14,18 +19,31 @@ export default function SignUp() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const res = await fetch("/api/auth/signup", {
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
-    });
-    const data = await res.json();
-    console.log(data);
+      });
+      const data = await res.json();
+      if(data.success === false) {
+        setLoading(false)
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
   };
-
   console.log(formData);
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold
@@ -38,9 +56,11 @@ export default function SignUp() {
           className="border p-3 rounded-lg" id="email" onChange={handleChange}/>
         <input type="password" placeholder="password"
           className="border p-3 rounded-lg" id="password" onChange={handleChange}/>
-        <button className="bg-slate-700 text-white p-3
+        <button disabled={loading} className="bg-slate-700 text-white p-3
         rounded-lg uppercase hov:opacity-95 
-        desiabled:opacity-80">Sign Up</button>
+        desiabled:opacity-80">
+          {loading ? "Loading..." : "sign Up"}
+        </button>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
@@ -48,6 +68,7 @@ export default function SignUp() {
           <span className="text-blue-700">Sign in</span>
         </Link>
       </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
