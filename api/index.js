67e2ea382d34d7139251a8b6a72ac6a2106ccc4b
Ray Devera <rayd808@yahoo.com>
2024-07-09 15:04:05 -0700

    create sign up API route

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-// import authRouter from "./routes/auth.route.js";
+import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 mongoose
@@ -23,14 +23,14 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRouter);
-// app.use("/api/auth", authRouter);
+app.use("/api/auth", authRouter);
 
-// app.use((err, req, res, next) => {
-//   const statusCode = err.statusCode || 500;
-//   const message = err.message || "Internal Server Error";
-//   return res.status(statusCode).json({
-//     success: false,
-//     statusCode,
-//     message,
-//   });
-// });
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
