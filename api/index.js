d87ca89224b59195ce11984985c280762764eb42
Ray Devera <rayd808@yahoo.com>
2024-07-16 17:50:33 -0700

    add create listing API route

--- api/index.js
+++ api/index.js
@@ -1,15 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
-import dotenv from "dotenv";
-import userRouter from "./routes/user.route.js";
-import authRouter from "./routes/auth.route.js";
-import cookieParser from "cookie-parser";
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+import listingRouter from './routes/listing.route.js';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose
   .connect(process.env.MONGO_DB_URI)
   .then(() => {
-    console.log("Connected to MongoDB!");
+    console.log('Connected to MongoDB!');
   })
   .catch((err) => {
     console.log(err);
@@ -18,20 +19,20 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
 app.use(cookieParser());
 
 app.listen(3000, () => {
-  console.log("Server is running on port 3000!");
+  console.log('Server is running on port 3000!');
 });
 
-app.use("/api/user", userRouter);
-app.use("/api/auth", authRouter);
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-// middleware that eliminates error handling for all routes. 
-// Using the middlware by passing the parameter next to the function in auth.controller.js:
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message || "Internal Server Error";
+  const message = err.message || 'Internal Server Error';
   return res.status(statusCode).json({
     success: false,
     statusCode,
